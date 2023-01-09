183e99ef8bb5cd1bd9742e710b96e59e72f382a2
andreastheger <109189094+andreastheger@users.noreply.github.com>
2023-01-09 10:22:59 -0800

    Add files via upload

---
+++ quake.js
@@ -0,0 +1,117 @@
+// Define earthquakes GeoJSON url variable
+let earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
+
+  switch(true) {
+    case earthquakesURL.includes("_day"):
+      periodText = "past 24 hours.";
+      break;
+    case earthquakesURL.includes("_week"):
+      periodText = "past 7 days.";
+      break;
+    case earthquakesURL.includes("_month"):
+      periodText = "past 30 days.";
+      break;
+              }
+
+d3.json(earthquakesURL, function(earthquakeData) {
+  // Get marker size from magnitide and mulitply by 4 to increase size on map
+  function markerSize(magnitude) {
+    return magnitude * 4;
+  };
+
+
+// Create layerGroup
+let earthquakes = L.layerGroup();
+
+
+// Define tile layer
+let grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
+});
+
+// Create a baseMaps object
+let baseMaps = {
+ "Grayscale Map": grayscaleMap
+  };
+
+
+// Create the map
+let myMap = L.map("mapid", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 2,
+  layers: [grayscaleMap, earthquakes]
+});
+
+// Create a layer control
+// Pass in the baseMaps and overlayMaps
+// Add the layer control to the map
+L.control.layers(baseMaps).addTo(myMap);
+
+  // Determine the marker color by depth
+  function chooseColor(depth) {
+    switch(true) {
+      case depth > 90:
+        return "red";
+      case depth > 70:
+        return "orangered";
+      case depth > 50:
+        return "orange";
+      case depth > 30:
+        return "gold";
+      case depth > 10:
+        return "yellow";
+      default:
+        return "lightgreen";
+    }
+  }
+
+  // Create a GeoJSON layer containing the features array
+  // Each feature a popup describing the place and time of the earthquake
+  L.geoJSON(earthquakeData, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, 
+        // Set the style of the markers based on properties.mag
+        {
+          radius: markerSize(feature.properties.mag),
+          fillColor: chooseColor(feature.geometry.coordinates[2]),
+          fillOpacity: 0.7,
+          color: "black",
+          stroke: true,
+          weight: 0.5
+        }
+      );
+    },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: "
+      + new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p><hr><p>Depth: "
+       + feature.geometry.coordinates[2] + " km" + "</p><hr><p>Date range: " + periodText + "</p>");
+    }
+  }).addTo(earthquakes);
+  // Sending our earthquakes layer to the createMap function
+  earthquakes.addTo(myMap);
+
+
+    // Add legend
+    let legend = L.control({position: "bottomright"});
+    legend.onAdd = function() {
+      let div = L.DomUtil.create("div", "info legend"),
+      depth = [-10, 10, 30, 50, 70, 90];
+      
+      div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+  
+      for (var i =0; i < depth.length; i++) {
+        div.innerHTML += 
+        '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
+            depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+        }
+        return div;
+      };
+      legend.addTo(myMap);
+});
